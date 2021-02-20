e1960680778019912fb0dfe33006cf09dc260804
Gabriela Izidorio <gabrielaizidorio18@gmail.com>
2021-02-20 16:49:39 +0000

    Update switch, continue and break

---
+++ intermediary-php/switch-continue-and-break.php
@@ -0,0 +1,23 @@
+<?php
+
+    // $name = 'Gabriela';
+
+    // switch($name) {
+    //     case 'Gabriela':
+    //         echo 'Minha variável é Gabriela';
+    //     break;
+
+    //     case 'Izabela':
+    //         echo 'Minha variável é Izabela';
+    //     break;
+    // }
+
+    // 'break' can used for 'switch, foreach, loop for, while and do' 
+
+        for($i = 0; $i < 10; $i++) {
+            if($i == 5)
+                continue;
+            echo $i;
+            echo '<hr>';
+        }
+?>
