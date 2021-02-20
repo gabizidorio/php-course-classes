7274bc6bbb8a7197d4478125f17fb0f9e5a3bd16
Gabriela Izidorio <gabrielaizidorio18@gmail.com>
2021-02-20 13:28:45 +0000

    Update 20/02

---
+++ intermediary-php/die-and-sleep.php
@@ -0,0 +1,11 @@
+<?php
+    $name = 'Gabriela';
+
+    if($name == 'Ana'){
+        echo 'Ok';
+    }else {
+        die("The script stopped running");
+    }
+
+    sleep(3);
+?>
---
+++ intermediary-php/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+    function showName($name, $age) {
+        echo '<h3>My name is ' .$name. ' </h3>';
+        echo '<hr>';
+        echo '<h3>I\'m </h3>'.$age.' years old';
+    }
+
+    showName('Gabriela', 20)
+
+?>
